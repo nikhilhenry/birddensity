3b1d48e2930919d56dc920f0cb96bec720c13e1c
nikhil-a10 <25770025+nikhilhenry@users.noreply.github.com>
2024-06-26 18:05:29 +0530

    complete base train script

---
+++ src/engine.py
@@ -0,0 +1,118 @@
+"""
+Contains functions for training and testing a PyTorch model.
+"""
+
+import torch
+from tqdm.auto import tqdm
+from typing import Dict, List, Tuple
+from torchmetrics.classification import BinaryAccuracy
+
+
+def train_step(
+    model: torch.nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    loss_fn: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    device: torch.device,
+) -> Tuple[float, float]:
+    """Trains a PyTorch model for a single epoch"""
+
+    model.train()
+
+    train_loss, train_acc = 0, 0
+
+    metric = BinaryAccuracy().to(device)
+
+    with tqdm(enumerate(dataloader), unit="batch", total=len(dataloader)) as tepoch:
+        for batch, (X, y) in tepoch:
+            X, y = X.to(device), y.to(device)
+
+            y_logits = model(X)
+
+            loss = loss_fn(y_logits, y)
+            train_loss += loss.item()
+
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            optimizer.step()
+
+            train_acc += metric(y_logits, y)
+
+            tepoch.set_postfix(accuracy=train_acc.item() / (batch + 1))
+
+    # Adjust metrics to get average loss and accuracy per batch
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+
+def test_step(
+    model: torch.nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    loss_fn: torch.nn.Module,
+    device: torch.device,
+) -> Tuple[float, float]:
+    """Tests a PyTorch model for a single epoch."""
+    model.eval()
+
+    test_loss, test_acc = 0, 0
+
+    metric = BinaryAccuracy().to(device)
+
+    with torch.inference_mode():
+        for batch, (X, y) in enumerate(dataloader):
+            X, y = X.to(device), y.to(device)
+
+            test_pred_logits = model(X)
+
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            test_acc += metric(test_pred_logits, y)
+
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+def train(
+    model: torch.nn.Module,
+    train_dataloader: torch.utils.data.DataLoader,
+    test_dataloader: torch.utils.data.DataLoader,
+    optimizer: torch.optim.Optimizer,
+    loss_fn: torch.nn.Module,
+    epochs: int,
+    device: torch.device,
+) -> Dict[str, List]:
+    """Trains and tests a PyTorch model."""
+
+    results = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+
+    for epoch in range(epochs):
+        train_loss, train_acc = train_step(
+            model=model,
+            dataloader=train_dataloader,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            device=device,
+        )
+        test_loss, test_acc = test_step(
+            model=model, dataloader=test_dataloader, loss_fn=loss_fn, device=device
+        )
+
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+
+    return results
---
+++ src/train.py
@@ -0,0 +1,63 @@
+import torch
+from pathlib import Path
+from dataset import CapuchinBirdCallDataset
+from torch.utils.data import DataLoader
+from models import EfficientNetB0Classifier
+import engine
+
+# Setup device-agnostic code
+if torch.cuda.is_available():
+    device = "cuda"  # NVIDIA GPU
+    torch.cuda.empty_cache()
+elif torch.backends.mps.is_available():
+    device = "mps"  # Apple GPU
+else:
+    device = "cpu"  # Defaults to CPU if NVIDIA GPU/Apple GPU aren't available
+
+print(f"Using device: {device}")
+
+torch.backends.cudnn.benchmark = True
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+WORKERS = 0
+
+DATA_FOLDER = Path("./data")
+CAPUCHIN_CLIPS_FOLDER = DATA_FOLDER / "Parsed_Capuchinbird_Clips"
+NOT_CAPUCHIN_CLIPS_FOLDER = DATA_FOLDER / "Parsed_Not_Capuchinbird_Clips"
+if not CAPUCHIN_CLIPS_FOLDER.exists() and not NOT_CAPUCHIN_CLIPS_FOLDER.exists():
+    raise Exception("Data folder not does not exist")
+
+SAMPLE_RATE = 16000
+NUM_FRAMES = 48000
+
+capuchin_call_dataset = CapuchinBirdCallDataset(
+    CAPUCHIN_CLIPS_FOLDER, NOT_CAPUCHIN_CLIPS_FOLDER, SAMPLE_RATE, NUM_FRAMES
+)
+
+train_test_split_generator = torch.Generator().manual_seed(42)
+train_dataset, test_dataset = torch.utils.data.random_split(
+    capuchin_call_dataset, [0.8, 0.2], generator=train_test_split_generator
+)
+
+train_dataloader = DataLoader(
+    dataset=train_dataset, batch_size=32, num_workers=WORKERS, shuffle=True
+)
+test_dataloader = DataLoader(
+    dataset=test_dataset, batch_size=32, num_workers=WORKERS, shuffle=True
+)
+
+model = EfficientNetB0Classifier().to(device)
+
+loss_fn = torch.nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+engine.train(
+    model,
+    train_dataloader,
+    test_dataloader,
+    optimizer,
+    loss_fn,
+    epochs=18,
+    device=device,
+)
